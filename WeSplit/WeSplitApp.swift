aaa894f9d56db8d30bb877aeaa2a3b0c800d1ed2
Aniket Sharma <aniket8439@gmail.com>
2024-04-09 13:05:35 +0530

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Aniket Sharma on 09/04/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
